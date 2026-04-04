2d11b2be3cd0697503c45c9113a80a663781ba21
Agapi Kallinikou <agapi.kallinikou@gmail.com>
2026-04-04 22:54:37 +0300

    Add Fahrenheit to Celsius conversion script

---
+++ 01_Intro_and_Basic/tempConv2.m
@@ -0,0 +1,6 @@
+% Interactive Fahrenheit to Celsius conversion
+f = input('Enter the temperature in Fahrenheit: ');
+c = (f - 32) * 5 / 9;
+
+disp('The temperature in Fahrenheit is:'), disp(f);
+disp('The temperature in Celsius is:'), disp(c);
