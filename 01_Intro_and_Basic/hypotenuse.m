2314ae0e2ee96053b14fee452a1775fa3e1649c7
Agapi Kallinikou <agapi.kallinikou@gmail.com>
2026-04-04 22:53:43 +0300

    Add hypotenuse calculation using Pythagorean theorem

---
+++ 01_Intro_and_Basic/hypotenuse.m
@@ -0,0 +1,8 @@
+% Calculate the hypotenuse of a right-angled triangle using the Pythagorean theorem
+a = input('Enter the length of the first perpendicular side: ');
+b = input('Enter the length of the second perpendicular side: ');
+
+c = sqrt(a^2 + b^2);
+
+disp('The length of the hypotenuse is:');
+disp(c);
